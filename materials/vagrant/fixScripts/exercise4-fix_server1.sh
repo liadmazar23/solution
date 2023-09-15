678e3992c9a555d803702a9e9e67e7957f795250
liadmazar23 <139764769+liadmazar23@users.noreply.github.com>
2023-09-15 13:53:49 +0300

    exercise4-fix_server1.sh

--- materials/vagrant/fixScripts/exercise4-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server1.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-#add fix to exercise4-server1 here
+#adding rule to hosts file
+sudo sed -i '$a\192.168.60.11      server2 server2' /etc/hosts
+
+# changing PasswordAuthentication value to 'yes'
+sudo sed -i "s:PasswordAuthentication no:PasswordAuthentication yes :g"   \
+  /etc/ssh/sshd_config
+
+# restart service so changes will take effect
+sudo service sshd restart
